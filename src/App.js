3a166bb4683bf0b89fd33326340f5773f064e832
Arseniy Semenenko <arseniysemenenko65@gmail.com>
2024-04-18 02:52:53 +0300

    Remove logo and favicon, add image switcher to App component
    * Delete favicon.ico, logo192.png, logo512.png from public folder
    * Remove App logo and related CSS in App.css
    
    Solve task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
-
+import img1 from "./images/logo512.png";
+import img2 from "./images/flowchart.png";
+import img3 from "./images/rocket-launch.png";
+import { useState } from 'react';
 function App() {
+  const [img , setImg] = useState(img1);
   return (
     <div className="App">
-      
+      <div>
+      <img
+        height="200px"
+        src={img}
+      ></img>
+      </div>
+      <div className='button'>
+        <button
+          onClick={async () => {
+            setTimeout(() => {
+              const imgs = [img1 , img2, img3];
+              const num = Math.floor(Math.random() * 3);
+              setImg(imgs[num]);
+            } , 3000);
+          }}
+        >Change</button>
+      </div>
     </div>
   );
 }
